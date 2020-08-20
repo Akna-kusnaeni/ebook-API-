546604dab85613923114e3569ce5e6d3021f932c
Your Name <you@example.com>
2020-08-20 19:27:06 +0700

    upload endpoint

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function me(){
+        return "NIS : 3103118008
+        Nama : Akna Kusnaeni
+        Gender : Perempuan
+        Phone : 085540260402
+        Kelas : XII RPL 1";
+    }
+}
